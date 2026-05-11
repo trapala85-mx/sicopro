daade59aaa2ef7e51ea0cf94660c90a97541b1a7
Ray <trapala1985@gmail.com>
2026-05-11 13:41:28 -0600

    feat: ProjectService se implementa capa de Servicio para Project.

--- backend/apps/launcher/admin.py
+++ backend/apps/launcher/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import Project, ProjectType
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active","created_at", "updated_at",)
+    search_fields = ("name","is_active")
+    list_filter = ("name", )
+
+
+@admin.register(ProjectType)
+class ProjectTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at",)
+    search_fields = ("name",)
+    list_filter = ("name", )
--- backend/apps/launcher/views/project_list_view.py
+++ backend/apps/launcher/views/project_list_view.py
@@ -1,43 +1,27 @@
 from rest_framework.views import APIView
-from apps.launcher.models import Project
 from apps.launcher.serializers import ProjectOutputSerializer
 from rest_framework import status
 from apps.core.utils import str_to_bool
 from apps.core.utils import success_response
 from apps.launcher.enums import ProjectListViewEnums
-
+from apps.launcher.services import ProjectService
 class ProjectListview(APIView):
 
     def get(self, request):
         """
-        1. Verificar si can_show viene
+        1. Verificar si can_show viene.
         """
-        can_show = request.query_params.get('can_show', None)
-        can_show = str_to_bool(can_show)
-
+        can_show = str_to_bool(request.query_params.get('can_show', None))
+        msg = ProjectListViewEnums.GET_ALL_ACTIVE_PROJECTS if can_show else ProjectListViewEnums.GET_ALL_PROJECTS
+        
         """
-        2. Hacmoes la búsqueda, si can_show es False que traiga todos si es True solo
-        los activos. Esto es usando el modelo Project lo cual nos devolverá la queryset, 
-        no los datos aún.
+        2. Delegamos al Service la lógica para obtener los Queryset.
         """
-        msg = ""
-        if not can_show:
-            projects = Project.objects.all()
-            msg = ProjectListViewEnums.GET_ALL_PROJECTS
-        else:
-            projects = Project.objects.all().filter(is_active=True)
-            msg = ProjectListViewEnums.GET_ALL_ACTIVE_PROJECTS
-
-        """
-        3. Necesitamos obtener los datos de manera correcta, para esto usamos el
-        Serializer que nos devolverá serializer listo para cnvertir 1 o varios
-        objetos. (para varios se debe incluir many=True)
-        """
-        serializer = ProjectOutputSerializer(instance=projects, many=True)
-
+        projects = ProjectService.get_projects(can_show=can_show)
         """
         4. Obtener el diccionario de python limpio con el Serializer
         """
+        serializer = ProjectOutputSerializer(instance=projects, many=True)
         projects_list = serializer.data
         
         return success_response(
